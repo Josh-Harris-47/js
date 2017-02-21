c4de7c4cf505c71b74c2fa56fe86380101b25d39
Josh-Harris-47 <joshuajamesharris@gmail.com>
2017-02-20 19:12:54 -0700

    closure function update

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
-var counter = function(){
-  var start = 1;
-  var count = count;
-  if(count == NaN){
-    console.log(start);
-    count = start;
-  }else{
-    console.log(count);
-  }
-  count++;
-  alert("counter was clicked");
+// var counter = function(){
+//   var start = 1;
+//   var count = count;
+//   if(count == NaN){
+//     console.log(start);
+//     count = start;
+//   }else{
+//     console.log(count);
+//   }
+//   count++;
+//   alert("counter was clicked");
+// };
+// $(".box").click(counter);
+///////////////////////////////////
+var outer = function(){
+  counter = 0;
+  return function(){
+    counter++;
+    console.log(counter);
+  };
 };
-$(".box").click(counter);
+var countUp = outer();
+countUp();-->1
+countUp();-->2
+countUp();-->3
