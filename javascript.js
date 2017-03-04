bbd48c0dbe2366640f82625fa426ad16c7a7c115
Josh-Harris-47 <joshuajamesharris@gmail.com>
2017-03-04 11:17:02 -0700

    time function

--- javascript.js
+++ javascript.js
@@ -1,18 +1,9 @@
-function(num1,num2){
- var val = [];
- var small = 0;
- // var one = 0;
- // var two = 0;
- if(num1 < num2){
-   small = num1;
- }else{
-   small = num2;
- }
- for(var i = 0; i <= small; i++){
-   if(num1%i == num2%i){
-   val.push(i);
- }
- }
- return val;
-};
-console.log(commondenominator(9,18));
+setInterval(function(){
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  var time = d.toLocaleTimeString();
+  console.log(time);
+  document.getElementsByClassName('circle').innerText = time;
+}, 1000);
